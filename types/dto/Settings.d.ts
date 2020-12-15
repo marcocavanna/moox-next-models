3388010b3a45da789ed50812a5d6d24a0fb97c83
Marco Cavanna <marco@appbuckets.io>
2020-12-15 15:15:29 +0100

    feat(settings): manage registry types

--- types/dto/Settings.d.ts
+++ types/dto/Settings.d.ts
@@ -1,7 +1,20 @@
+import { DtoType } from './_utils';
+
+
 export namespace Settings {
 
   export interface RegistryType {
+    description: DtoType.Nullable<string>;
 
+    isCustomer: boolean;
+
+    isIdle: boolean;
+
+    isProspect: boolean;
+
+    isSupplier: boolean;
+
+    name: string;
   }
 
 }
